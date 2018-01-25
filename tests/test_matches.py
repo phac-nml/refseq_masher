dd683767985900a706d508e78b3a35dc6ef258a9
pkruczkiewicz <peter.kruczkiewicz@canada.ca>
2018-01-25 17:01:10 -0600

    add ipynb for how taxonomy info was retreived WIP: adding tests updated travis.yml
    Signed-off-by: pkruczkiewicz <peter.kruczkiewicz@canada.ca>

--- tests/test_matches.py
+++ tests/test_matches.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+import os
+from click.testing import CliRunner
+import pytest
 
-def test_something():
-    assert 1 + 1 == 2
+from refseq_masher import __version__
+import refseq_masher.cli as cli
+
+@pytest.fixture(scope='module')
+def runner():
+    return CliRunner()
+
+def test_print_version(runner):
+    result = runner.invoke(cli.cli, ['--version'])
+    assert result.exit_code == 0
+    assert __version__ in result.output
+
+def test_fasta(runner):
+    fasta = 'tests/data/contigs.fasta'
+    result = runner.invoke(cli.matches, [fasta])
+    print(result)
+    assert result.exit_code == 0
+    print(result.output)
